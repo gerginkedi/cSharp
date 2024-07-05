296e1d315c59ec80e5ae860a106592322ff8288e
kedi <98282468+gerginkedi@users.noreply.github.com>
2024-07-05 20:10:38 +0300

    classlar
    classlar referabns tipli degiskenler

---
+++ classlar/classlar/Ornek.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classlar
+{
+    internal class Ornek
+    {
+        //siniflar nesne uretmemizi saglayan yapilardir
+        //siniflar int, string gibi bir tiptir
+        //referans tipli degiskenlerde tip bir sinif olmaliir
+    }
+}
---
+++ classlar/classlar/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace classlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 0;
+            Ornek abc = new Ornek();
+            //nesneler herzaman heapde olusturulur
+            //stackdeki bir referans noktasindan heapdeki nesneye gider
+            new Ornek(); //bunu bir referansi yoktur. dogurdan erisilemez
+            //garbage collector referansi olmayan nesneleri siler
+            Ornek x;
+            x = new Ornek();
+            //once referans uretilir sonra objesi uretilir
+            Ornek y = new Ornek();
+            Ornek k = y;
+            //y a objesini gosteriyosa k da ayni objeyio gosteriyo
+            int j = 0;
+            int m = j;
+            //burda deger kopyalanarak yazilir 
+            if (y.Equals(k))
+            {
+                Console.WriteLine("esit");
+            }
+            else
+            {
+                Console.WriteLine( "degil");
+            }
+        }
+    }
+}
