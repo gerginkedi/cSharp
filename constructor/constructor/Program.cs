a473fba2c8445b5a6d70b7cd809e8439a7b50bcb
kedi <98282468+gerginkedi@users.noreply.github.com>
2024-07-08 19:30:08 +0300

    constructor
    c# constructor

---
+++ constructor/constructor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.InteropServices;
+
+namespace constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ornek o = new Ornek();
+            int[] yaslar = new int[4];
+            //burda object uretim asamasinda ik itane parantez acip kapatiyosak varysailan constructordir
+        }
+
+        
+        class Ornek
+        {
+
+            
+            #region Constructor
+            //constructor metod public olmalidir
+            //construcotr metodun geri donus degferi olmaz
+            //construcor metodun ismi class ismi ile ayni olmak zorundadir
+            //geri donus olmiyacagi icin void yazilmasi beklenir ama o da yazilmaz 
+            //bir siniftan onject uretilirkenherseyedne once construcotr tetiklenir
+            //constructor overload olabilir
+            public int x { get; set; }
+            public Ornek() // constructor metod
+            {
+                x = 15;
+            }
+
+            #endregion
+
+            #region Static Constructor
+            #endregion
+        }
+    }
+}
---
+++ constructor2/constructor2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace constructor2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ornek k = new Ornek();
+            Console.WriteLine(k.x);
+            
+        }
+
+        class Ornek
+        {
+            public int x { get; set; }
+            
+
+            public Ornek() 
+            {
+                x = 12;
+               
+            }
+        }
+    }
+}
