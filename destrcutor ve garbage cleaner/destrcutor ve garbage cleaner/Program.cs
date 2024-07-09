52c2f1e8f66cae71e52333aa1bd1cdc19715d618
kedi <98282468+gerginkedi@users.noreply.github.com>
2024-07-09 12:25:01 +0300

    indexer, garbage collector ve destructor

---
+++ destrcutor ve garbage cleaner/destrcutor ve garbage cleaner/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace destrcutor_ve_garbage_cleaner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++) 
+            {
+                new Ornek(i);
+                
+            }
+            Console.WriteLine("**************");
+            
+            GC.Collect();
+            Console.Read();// olmayinca calismiyo
+        }
+
+
+        class Ornek 
+        {
+
+            int numara;
+            public Ornek(int id)
+            {// constructor
+                numara = id;
+                Console.WriteLine(numara + " idli nesne uretildi");
+            }
+
+
+
+        }
+    }
+}
---
+++ indexer/indexer/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace indexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human = new Human();
+            human[1] = "omer";
+            human[2] = "ahmet";
+            Console.WriteLine(human[1]);
+            Console.WriteLine(human[2]);
+
+        }
+    }
+    class Human
+    {
+        string[] names = new string[10];
+        public string this[int i]
+        {
+            
+            get 
+            {
+                return names[i];
+            }
+            set 
+            {
+                names[i] = value;      
+            }
+
+            
+        }
+    }
+}
